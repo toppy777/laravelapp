05d12d4a3453f00fb46b651eb783befab71e519c
toppy777 <toppy01257@yahoo.co.jp>
2021-10-08 12:36:29 +0000

    boot()からエラーが出る箇所を削除

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\json\JsonResource;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        JsonResource::withoutWrapping();
+        Schema::defaultStringLength(191);
     }
 }
